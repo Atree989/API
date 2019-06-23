6aa94c3c9b71cf1055d260505c25dadc40fa66c4
Atree989 <47877127+Atree989@users.noreply.github.com>
2019-06-23 12:42:20 +0800

    Update repos_1.py

--- python_repos/repos_1.py
+++ python_repos/repos_1.py
@@ -7,7 +7,7 @@ import requests
 #执行API调用并存储响应
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
-print("Status code:",r.status_code)
+print("Status code:",r.status_code)#状态码200表示请求成功
 
 #将API响应存储在一个变量中
 response_dict = r.json()
